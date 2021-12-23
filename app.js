ceba5cd3c8576ffe9607b24a7cdb46f1530c9ab0
Nath <enath6.25@gmail.com>
2021-12-23 00:14:01 -0600

    button with event

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const color = document.querySelector(".color");
 
 btn.addEventListener("click",function(){
     
-    const randomNumber = 3;
+    const randomNumber = getRandomNumber();
+
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent= colors[randomNumber];
 });
+
+function getRandomNumber() {
+ return Math.floor(Math.random()* colors.length)
+};
